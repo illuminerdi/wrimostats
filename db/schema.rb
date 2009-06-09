1bded5e798b1f3394e038aaad2ffcb0016975cb6
Joshua Clingenpeel <joshua.clingenpeel@gmail.com>
2009-06-08 21:49:08 -0700

    adding extra xml fixtures. so much xml.

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090607231121) do
+ActiveRecord::Schema.define(:version => 20090609000348) do
+
+  create_table "buddies", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "uid"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "sessions", :force => true do |t|
     t.string   "session_id", :null => false
