60cc8d3fe07354c7ce386d5473243f830d34df23
Joshua Clingenpeel <joshua.clingenpeel@gmail.com>
2009-06-08 21:47:37 -0700

    getting buddies tested and implemented for unit work, using shoulda, which makes me feel dumb for not using it from the beginning.

--- app/models/buddy.rb
+++ app/models/buddy.rb
@@ -1,2 +1,17 @@
 class Buddy < ActiveRecord::Base
+  belongs_to :user
+  validates_presence_of :user_id, :uid
+  validates_uniqueness_of :uid, :scope => :user_id
+  validate :valid_uid?
+
+  private
+
+  def valid_uid?
+    return if uid.nil?
+    unless User.find_by_uid(uid)
+      nano_data = Nanowrimo::User.new uid
+      nano_data.load
+      errors.add(:uid, "Unable to find Nanowrimo ID") if nano_data.has_error?
+    end
+  end
 end
--- test/unit/buddy_test.rb
+++ test/unit/buddy_test.rb
@@ -1,8 +1,31 @@
 require 'test_helper'
 
 class BuddyTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  FakeWeb.allow_net_connect = false
+  reid = File.expand_path(File.dirname(__FILE__)) + "/../fixtures/xml/user_245095_wc.xml"
+  FakeWeb.register_uri("http://www.nanowrimo.org/wordcount_api/wc/245095", :file => reid)
+  renda = File.expand_path(File.dirname(__FILE__)) + "/../fixtures/xml/user_94450_wc.xml"
+  FakeWeb.register_uri("http://www.nanowrimo.org/wordcount_api/wc/94450", :file => renda)
+  me = File.expand_path(File.dirname(__FILE__)) + "/../fixtures/xml/user_wc.xml"
+  FakeWeb.register_uri("http://www.nanowrimo.org/wordcount_api/wc/240659", :file => me)
+  bad = File.expand_path(File.dirname(__FILE__)) + "/../fixtures/xml/user_wc_error.xml"
+  FakeWeb.register_uri("http://www.nanowrimo.org/wordcount_api/wc/999999", :file => bad)
+
+  fixtures :all
+
+  should_validate_uniqueness_of :uid, :scoped_to => :user_id
+  should_validate_presence_of :user_id, :uid
+  should_belong_to :user
+
+  context "A Buddy instance" do
+    setup do
+      @buddy = buddies(:one)
+    end
+
+    should "return false when sent #valid_uid?" do
+      @buddy.uid = 999999
+      assert ! @buddy.valid?
+      assert @buddy.errors.on(:uid)
+    end
   end
 end
