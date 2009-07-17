4bbbadba77bb627204c3cd1b074207f0de5696a1
Joshua Clingenpeel <joshua.clingenpeel@gmail.com>
2009-07-17 09:51:29 -0700

    building in more tests, looking for people who aren't willing participants or who aren't in a word war to make sure they can't comment.

--- test/unit/oh_snap_test.rb
+++ test/unit/oh_snap_test.rb
@@ -6,4 +6,22 @@ class OhSnapTest < ActiveSupport::TestCase
   should_belong_to :user
   should_belong_to :word_war
   should_validate_presence_of :word_war_id, :user_id, :comment
+
+  context "a new Oh Snap" do
+    setup do
+      @oh_snap = OhSnap.new({:word_war_id => word_wars(:two), :comment => "Simple comment"})
+    end
+
+    should "not allow snaps from a user not part of this word war" do
+      @oh_snap.user_id = 245095
+      assert ! @oh_snap.valid?
+      assert @oh_snap.errors.on(:word_war_id)
+    end
+
+    should "not allow snaps from a user who is not a willing participant" do
+      @oh_snap.user_id = 123456
+      assert ! @oh_snap.valid?
+      assert @oh_snap.errors.on(:word_war_id)
+    end
+  end
 end
